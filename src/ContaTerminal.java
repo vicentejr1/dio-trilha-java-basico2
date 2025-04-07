9ca5e3ec0cde2c378a201cc5b732779330603825
vicentejr1 <vicentejunior.ij@hotmail.com>
2025-04-07 01:13:22 -0300

    meu segundo commit do projeto

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -6,6 +6,7 @@ public class ContaTerminal {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        String nomeCliente;
 
         System.out.println("Prezado cliente, a VBANK agradece sua preferência! \n");
         System.out.println("\nPor favor, digite o número da conta: ");
@@ -15,12 +16,15 @@ public class ContaTerminal {
         String agencia = scanner.next();
 
         System.out.println("\nDigite seu nome: ");
-        String nomeCliente = scanner.next();
+        nomeCliente = scanner.nextLine();
+        nomeCliente = scanner.nextLine();
 
-        System.out.println("\nDigite o seu saldo: ");
+        System.out.println("\nDigite o seu saldo (utilize ponto para decimais): ");
         Float saldo = scanner.nextFloat();
 
         System.out.println("\nOlá " + nomeCliente + ", obrigado por criar uma conta em nosso banco! Sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo R$" + saldo + " já está disponível para saque.\n");
+
+        scanner.close();
 
 
 
